b5b15ef05acf2b19fcb88bcf5005d8c1f26fbaa8
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-09-14 20:58:44 -0500

    add changes

--- projects/requirements/infrastructure/persistence/mysql/requirements.repository.test.ts
+++ projects/requirements/infrastructure/persistence/mysql/requirements.repository.test.ts
@@ -1,26 +1,60 @@
 import "reflect-metadata";
-import { afterEach, beforeEach, expect, it } from "vitest";
 
-import { destroyContainer, initializeContainer } from "@/projects/container";
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { MockRequirementsRepository } from '@/projects/requirements/infrastructure/persistence/mysql/requirements.repository.mock';
+import { db } from '@/projects/shared/drizzle';
 
-beforeEach(() => {
-  initializeContainer();
-});
+describe('RequirementsMySqlRepository', () => {
+  let repository: MockRequirementsRepository;
 
-afterEach(() => {
-  destroyContainer();
-});
+  // Mock de las funciones del db
+  const mockSelect = vi.fn();
+  const mockInsert = vi.fn();
+  const mockUpdate = vi.fn();
+  const mockTransaction = vi.fn();
 
-it("returns requirements", async () => {
+  beforeEach(() => {
+    repository = new MockRequirementsRepository();
 
-  //1. Add mock to get requirements of the database with drizzle
+    // Restablecer los mocks antes de cada prueba
+    mockSelect.mockReset();
+    mockInsert.mockReset();
+    mockUpdate.mockReset();
+    mockTransaction.mockReset();
 
-  //2. Consuming method getRequirements from RequirementsMySqlRepository
+    // Simular el comportamiento de `db`
+    db.select = mockSelect;
+    db.insert = mockInsert;
+    db.update = mockUpdate;
+    db.transaction = mockTransaction;
+  });
 
-  //3. Validate the result of the method getRequirements
+  it('should return requirements list', async () => {
+    // Configura el mock para devolver los datos que esperas
+    mockSelect.mockResolvedValueOnce([
+      {
+        id: '1',
+        description: 'Requirement 1',
+        priority: 'HIGHT',
+        status: 'NEW',
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+        details: [
+          {
+            id: '1',
+            description: 'Detail 1',
+            quantity: '1',
+            requirement_id: '1',
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          }
+        ]
+      }
+    ]);
 
-  //4. Validate the result of the method getRequirements with pagination
+    const result = await repository.getRequirements({ page: 1, sizePage: 10 }, { search: '' });
 
-  expect(true).toBe(true);
-
+    // Asegúrate de que los datos devueltos coincidan con los esperados
+    expect(result.requirements.length).toBe(1);
+  });
 });
